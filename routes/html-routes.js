63c926b19bcd12a6b79fddb94f94f248e5a75f0e
Anya <anyaanto1909@gmail.com>
2020-02-04 13:51:57 -0500

    updated html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const path = require("path");
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
